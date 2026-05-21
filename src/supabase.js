9d466702e9b086f326c3a31fd764e1d8ecceeb2d
Paulus2k <pcardonas@miumg.edu.gt>
2026-05-21 09:20:54 -0600

    Fix supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://eameixtdkokjbabvhhtt.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhbWVpeHRka29ramJhYnZoaHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MTEyNDgsImV4cCI6MjA5NDE4NzI0OH0.VzEQUFlM_QNBDv9MCS9HJhsmgmf_XWDOShhBiYhG5Ro'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
